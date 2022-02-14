40941fb032cc0724e5ab53cf5d42643e7aabe648
peter.chen1024 <u0052017@gmail.com>
2022-02-14 15:32:25 +0800

    init

---
+++ javascript/all.js
@@ -0,0 +1,13 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.31/vue.esm-browser.min.js';
+
+
+const app = createApp({
+  data() {
+    return {
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'peter_vue2022',
+    }
+  },
+})
+
+app.mount('#app');
