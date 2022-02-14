d1f0bf9d8f77a88dc7df3920e0fb3b62a0f0ab93
peter.chen1024 <u0052017@gmail.com>
2022-02-14 17:50:43 +0800

    add productModal component

--- javascript/all.js
+++ javascript/all.js
@@ -1,4 +1,4 @@
-/* global axios */
+/* global axios,bootstrap */
 // eslint-disable-next-line
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.31/vue.esm-browser.min.js';
 
@@ -8,6 +8,8 @@ const app = createApp({
       apiUrl: 'https://vue3-course-api.hexschool.io/v2',
       apiPath: 'peter_vue2022',
       products: [],
+      cartData: [],
+      productId: '',
     };
   },
   methods: {
@@ -17,9 +19,73 @@ const app = createApp({
         this.products = res.data.products;
       });
     },
+    openProductModal(id) {
+      if (this.productId === id) {
+        this.$refs.productModal.openModal(); // productId相同直接開啟Modal
+      } else {
+        this.productId = id; // productId改變，重新取得product
+      }
+    },
+    getCart() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/cart`;
+      axios.get(url).then((res) => {
+        this.cartData = res.data.data;
+      });
+    },
+    addToCart(id, qty = 1) {
+      const data = {
+        product_id: id,
+        qty,
+      };
+      const url = `${this.apiUrl}/api/${this.apiPath}/cart`;
+      axios.post(url, { data }).then((res) => {
+        if (res.data.success) {
+          this.getCart();
+        }
+      });
+    },
   },
   mounted() {
     this.getProducts();
+    this.getCart();
+  },
+});
+
+app.component('productModal', {
+  template: '#userProductModal',
+  props: ['productId'],
+  data() {
+    return {
+      modal: null,
+      product: {},
+      apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+      apiPath: 'peter_vue2022',
+      qty: 1,
+    };
+  },
+  watch: {
+    productId() {
+      this.getProduct();
+    },
+  },
+  methods: {
+    openModal() {
+      this.modal.show();
+    },
+    getProduct() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/product/${this.productId}`;
+      axios.get(url).then((res) => {
+        this.product = res.data.product;
+        this.openModal();
+      });
+    },
+  },
+  mounted() {
+    this.modal = new bootstrap.Modal(this.$refs.modal, {
+      backdrop: true,
+      keyboard: true,
+      focus: true,
+    });
   },
 });
 
