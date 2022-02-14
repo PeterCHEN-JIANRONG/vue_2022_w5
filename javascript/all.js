7111e470686ae789483bb1beb3ebece82f2a5915
peter.chen1024 <u0052017@gmail.com>
2022-02-14 16:32:41 +0800

    add productList

--- javascript/all.js
+++ javascript/all.js
@@ -1,3 +1,5 @@
+/* global axios */
+// eslint-disable-next-line
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.31/vue.esm-browser.min.js';
 
 const app = createApp({
@@ -5,7 +7,19 @@ const app = createApp({
     return {
       apiUrl: 'https://vue3-course-api.hexschool.io/v2',
       apiPath: 'peter_vue2022',
+      products: [],
+    };
+  },
+  methods: {
+    getProducts() {
+      const url = `${this.apiUrl}/api/${this.apiPath}/products/all`;
+      axios.get(url).then((res) => {
+        this.products = res.data.products;
+      });
     },
+  },
+  mounted() {
+    this.getProducts();
   },
 });
 
